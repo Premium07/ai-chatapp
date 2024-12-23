acbb707219461d01a9704f6e18e178c9c5961e4a
Premium07 <premsagarg23@gmail.com>
2024-12-23 12:35:01 +0545

    add register route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,20 @@
-import userService from "../services/user.service.js";
+import createUserService from "../services/user.service.js";
+import { validationResult } from "express-validator";
 
-export const createUser = async (req, res) => {
+export const createUserController = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
-    const user = await userService.createUserService(req.body);
-    res.status(201).json(user);
+    const user = await createUserService(req.body);
+    const token = await user.generateAuthToken();
+
+    res.status(201).json(user, token);
   } catch (error) {
-    res.status(400).send(error.message);
+    console.error(error);
+    res.status(500).json({ errors: [{ msg: "Server error" }] });
   }
 };
