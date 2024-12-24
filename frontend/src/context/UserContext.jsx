8f7a5a0e27d11a8980148cb50d344b042f7950ac
Premium07 <premsagarg23@gmail.com>
2024-12-24 14:22:11 +0545

    add context api

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+// Create the UserContext
+const UserContext = createContext();
+
+// Create a provider component
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
