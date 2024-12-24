cecf62941bee7cacdc7347d88a507cdf21e256e9
Premium07 <premsagarg23@gmail.com>
2024-12-24 15:38:48 +0545

    fix: enhance uniqueness validation message for project name

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -6,7 +6,7 @@ const projectSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      unique: true,
+      unique: [true, "Project name must be unique"],
       lowercase: true,
     },
     users: [
