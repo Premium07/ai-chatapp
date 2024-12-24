860c31a8530a88dcbe21ddadba277e6b2ae4bdea
Premium07 <premsagarg23@gmail.com>
2024-12-24 15:13:08 +0545

    add middleware to project creattion

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { createProject } from "../controllers/project.controller.js	";
 import { body } from "express-validator";
+import { authUser } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post(
   "/create",
   body("name").isString().withMessage("Name is required"),
+  authUser,
   createProject
 );
 
