3229661d168c3ab061e4ab596a0318d903eb7529
Premium07 <premsagarg23@gmail.com>
2024-12-24 14:46:42 +0545

    add service for project

---
+++ backend/services/project.service.js
@@ -0,0 +1,15 @@
+import Project from "../models/project.model.js";
+
+export const createProject = async ({ name, userId }) => {
+  if (!name) {
+    throw new Error("Project name is required");
+  }
+
+  if (!userId) {
+    throw new Error("User id is required");
+  }
+
+  const project = await Project.create({ name, users: [userId] });
+
+  return project;
+};
