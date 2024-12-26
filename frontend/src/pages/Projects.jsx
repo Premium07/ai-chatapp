7e161704fac1d9ee90f5dd795c9f238fe907b57f
Premium07 <premsagarg23@gmail.com>
2024-12-26 13:48:35 +0545

    feat: enhance Projects page layout with user group button

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,10 +1,23 @@
 import { useLocation } from "react-router-dom";
+import { FaUserGroup } from "react-icons/fa6";
 
 const Projects = () => {
   const location = useLocation();
 
-  console.log(location.state);
-  return <div>Projects</div>;
+  // console.log(location.state);
+
+  return (
+    <main className="h-screen w-screen flex">
+      <section className="h-full min-w-60 bg-red-400">
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-200">
+          <button className="p-2">
+            <FaUserGroup />
+          </button>
+        </header>
+      </section>
+      <section></section>
+    </main>
+  );
 };
 
 export default Projects;
