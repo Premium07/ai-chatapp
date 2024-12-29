d7cbe5ca39db499e03c07e5bfb9e79ebeecf743f
Premium07 <premsagarg23@gmail.com>
2024-12-29 12:32:06 +0545

    feat: integrate Socket.IO for real-time communication in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import "dotenv/config";
 import http from "http";
 import app from "./app.js";
+import { Server } from "socket.io";
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+const io = new Server(server);
 
-const PORT = process.env.PORT || 5000;
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("event", (data) => {
+    /* … */
+  });
+  socket.on("disconnect", () => {
+    /* … */
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
