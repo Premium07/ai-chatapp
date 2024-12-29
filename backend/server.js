a0d055b5dc0cc4b46037afc7d4790a91bf0c880d
Premium07 <premsagarg23@gmail.com>
2024-12-29 13:48:52 +0545

    feat: enhance Socket.IO middleware to validate projectId and associate project with socket

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import app from "./app.js";
 import { Server } from "socket.io";
 const PORT = process.env.PORT || 5000;
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import Project from "./models/project.model.js";
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -12,11 +14,19 @@ const io = new Server(server, {
   },
 });
 
-io.use((socket, next) => {
+io.use(async (socket, next) => {
   try {
     const token =
       socket.handshake.auth?.token ||
       socket.handshake.headers.authorization?.split(" ")[1];
+
+    const projectId = socket.handshake.query.projectId;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+      return next(new Error("Invalid projectId"));
+    }
+
+    socket.project = await Project.findById(projectId);
 
     if (!token) return next(new Error("Authentication Error"));
 
@@ -34,12 +44,15 @@ io.use((socket, next) => {
 
 io.on("connection", (socket) => {
   console.log("user connected");
-  socket.on("event", (data) => {
-    /* … */
+
+  socket.join(socket.project._id);
+  socket.on("project-message", (data) => {
+    console.log(data);
+    socket.broadcast.to(socket.project._id).emit("project-message", data);
   });
-  socket.on("disconnect", () => {
-    /* … */
-  });
+
+  socket.on("event", (data) => {});
+  socket.on("disconnect", () => {});
 });
 
 server.listen(PORT, () => {
