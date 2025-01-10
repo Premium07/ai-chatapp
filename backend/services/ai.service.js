45d4a90ef712afcdd3ab777f759e95e4991e7acc
Premium07 <premsagarg23@gmail.com>
2025-01-10 14:09:30 +0545

    feat: refine AI model system instruction for improved response accuracy and maintainability

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -1,15 +1,16 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-  generationConfig: {
-    responseMimeType: "application/json",
-    temperature: 0.4,
-  },
-  systemInstruction: `You are an expert in Full Stack Development and Software Engineering and related to Tech Industry and in every programming languages like Python, C, C++, Java, JavaScript, Go, Android Devlopment, iOS Devlopment and many other languages and you ar trained in lots of ai models.You have an experience of 10 years of in the related field. You always write the code in modular and break the code in the possible way and follow best practices of the development, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previuos code. You always follows the best practices of development and engineering, You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle errors and expections. 
-  
-   Examples: 
+    model: "gemini-1.5-flash",
+    generationConfig: {
+        responseMimeType: "application/json",
+        temperature: 0.4,
+    },
+    systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.
+    
+    Examples: 
 
     <example>
  
@@ -97,12 +98,12 @@ const model = genAI.getGenerativeModel({
  IMPORTANT : don't use file name like routes/index.js
        
        
-    
-  
-  `,
+    `
 });
 
 export const generateResult = async (prompt) => {
-  const result = await model.generateContent(prompt);
-  return result.response.text();
-};
+
+    const result = await model.generateContent(prompt);
+
+    return result.response.text()
+}
